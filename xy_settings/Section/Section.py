2ffb7bfa9fd079cbf36265a5af4151f1cffb853e
yuyanget <845262968@qq.com>
2024-10-20 21:00:52 +0800

    release 1.0.2发布

--- xy_settings/Section/Section.py
+++ xy_settings/Section/Section.py
@@ -1,6 +1,14 @@
 # -*- coding: UTF-8 -*-
 __author__ = "helios"
 
+''' 
+ * @Author: yuyanget 845262968@qq.com
+ * @Date: 2024-10-19 18:34:54
+ * @LastEditors: yuyanget 845262968@qq.com
+ * @LastEditTime: 2024-10-20 20:58:01
+ * @FilePath: /helios/workspace/project/opensource/xy-base/xy_settings/xy_settings/Section/Section.py
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ '''
 from pathlib import Path
 from xy_file.File import File
 from xy_configure.Pair.Section import Section as xySection
@@ -44,8 +52,8 @@ class Section(xySection):
     def _make_path(self, path: Path, auto_create_path: bool = False) -> Path | None:
         if not isinstance(path, Path):
             return None
-        path = self.absolute_path(path=path)
         if auto_create_path and isinstance(path, Path):
+            path = self.absolute_path(path=path)
             if path.suffix:
                 path = File.touch(path)  # type: ignore
             else:
